9183677df70e3197a45ca7d8063b1a10697ed688
Geek1-mt <2503941193@qq.com>
2021-06-17 20:07:13 +0800

    first commit

---
+++ HotelManagement/src/com/ziyi/test/SaleMachineTest.java
@@ -0,0 +1,61 @@
+package com.ziyi.test;
+
+import org.junit.Before;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class SaleMachineTest {
+	SaleMachine t =new SaleMachine();
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/*@Test
+	public void test() {
+		
+		t.sale("beer", 3);
+	
+		
+	}*/
+	@Test
+	public void test1() {
+		/*t.sale("beer", 3);
+		t.sale("cola", 5);
+		t.sale("beer",10 );
+		t.sale("beer", 5);
+		t.sale("beer", 10);
+		t.sale("beer", 10);
+		t.sale("beer", 10);
+		t.sale("beer", 10);
+		t.sale("orange", 8);
+		t.sale("orange", 10);
+		t.sale("orange", 5);
+		t.sale("orange", 10);
+		t.sale("orange", 5);
+		t.sale("orange", 10);*/
+		
+		
+t.sale("beer", 3);
+t.sale("cola", 5);
+t.sale("beer", 10);
+t.sale("beer", 10);
+t.sale("orange", 10);
+t.sale("beer", 10);
+t.sale("orange", 10);
+t.sale("beer", 10);
+t.sale("orange", 10);
+t.sale("beer", 10);
+t.sale("orange", 10);
+t.sale("beer", 5);
+t.sale("beer", 5);
+t.sale("beer", 5);
+t.sale("orange", 0);
+t.sale("orange", 10);
+t.sale("orange", 10);
+t.sale("orange", 10);
+
+	}
+  
+}
